9511fe4025303bfe0ec452591b7f2f4c691b6b74
Chirantha Kurukulasuriya <chiranthajk@gmail.com>
2022-02-13 18:22:46 +0530

    fixed minor issues

--- warlocks/controllers/Combined/Combined.cpp
+++ warlocks/controllers/Combined/Combined.cpp
@@ -1,27 +1,23 @@
 #include <webots/DistanceSensor.hpp>
 #include <webots/Motor.hpp>
 #include <webots/Robot.hpp>
+#include <webots/Camera.hpp>
 
 #define TIME_STEP 16
-
 #define normal_speed 6
 
 using namespace webots;
 
 Robot *robot = new Robot();
-Motor *motors[2] = {robot->getMotor("left_motor"),robot->getMotor("right_motor")};
+Motor *motors[2] = {robot->getMotor("left_motor"), robot->getMotor("right_motor")};
 DistanceSensor *us[3];
 DistanceSensor *ir[8];
 Camera *cm = robot->getCamera("cam");
 
-
-
-char irNames[8][5] = {"ir1","ir2","ir3","ir4","ir5","ir6","ir7","ir8"};
+char irNames[8][5]  = {"ir1","ir2","ir3","ir4","ir5","ir6","ir7","ir8"};
 char usNames[3][10] = {"us_left","us_front","us_right"};
-double left_motor =0.0;
-double right_motor=0.0;
-
-
+double left_motor = 0.0;
+double right_motor= 0.0;
 
 void delay(float t);
 void turn_left();
@@ -38,6 +34,7 @@ int main(int argc, char **argv) {
   }
   
   cm->enable(TIME_STEP);
+  
   while (robot->step(TIME_STEP) != -1) {
     follow_continous_line();
     delay(500);
@@ -47,17 +44,130 @@ int main(int argc, char **argv) {
   
 }
   
+//////////////////////////////////////////////////////// 
+void follow_continous_line(){
 
+  int ir_readings[8];
+  double kp = 3;
+  double kd = 0.2;
+  double ki = 0.01;
+  double previous_error = 0.0;
+  double integral = 0.0;
+  int coefficient[8] = {-4000,-3000,-2000,-1000,1000,2000,3000,4000};
+   
+   
+  for (int i = 0; i < 8; i++) {
+    ir[i] = robot->getDistanceSensor(irNames[i]);
+    ir[i]->enable(TIME_STEP);
+  }
+  
+  while (robot->step(TIME_STEP) != -1) {
+    
+    bool exit = true;
+    
+    for (int i = 0; i < 8; i++) {
+        if (ir[i]->getValue() < 150){
+          ir_readings[i]=0;
+          
+          }
+        else{
+          ir_readings[i]=1;
+          exit = false;
+        }  
+    } 
+    
+    if (exit){break;}
+   
+    double error = 0.0;
+    for (int j=0; j<8;j++){
+      error=error+ir_readings[j]*coefficient[j];
+    }
+    
+    double P= kp*error;
+    double I= (ki*error)+integral;
+    double D= kd*(error-previous_error);
+    
+    
+    double correction = (P+I+D)/1000;
+    double left_motor = normal_speed + correction;
+    double right_motor = normal_speed - correction;
+    
+    if (left_motor < 0.0)  {left_motor=0.0;}
+    if (left_motor > 10.0) {left_motor=10.0;}
+    if (right_motor< 0.0)  {right_motor=0.0;}
+    if (right_motor> 10.0) {right_motor=10.0;}
+    
+    
+    motors[0]->setVelocity(left_motor);
+    motors[1]->setVelocity(right_motor);
+    
+    previous_error = error;
+    integral = I;  
+  }
 
+}
 
+void follow_dashed_line(){
 
+  int ir_readings[8];
+  double kp = 3;
+  double kd = 0.2;
+  double ki = 0.01;
+  double previous_error = 0.0;
+  double integral = 0.0;
+  int coefficient[8] = {-4000,-3000,-2000,-1000,1000,2000,3000,4000};
+   
+   
+  //for (int i = 0; i < 8; i++) {
+  //  ir[i] = robot->getDistanceSensor(irNames[i]);
+  //  ir[i]->enable(TIME_STEP);
+  //}
+  
+  while (robot->step(TIME_STEP) != -1) {
+    
+    bool exit = true;
+    
+    for (int i = 0; i < 8; i++) {
+        if (ir[i]->getValue() < 150){
+          ir_readings[i]=0;
+          
+          }
+        else{
+          ir_readings[i]=1;
+          exit = false;
+        }  
+    } 
+    
+    if (exit){break;}
+   
+    double error = 0.0;
+    for (int j=0; j<8;j++){
+      error=error+ir_readings[j]*coefficient[j];
+    }
+    
+    double P= kp*error;
+    double I= (ki*error)+integral;
+    double D= kd*(error-previous_error);
+    
+    
+    double correction = (P+I+D)/1000;
+    double left_motor = normal_speed + correction;
+    double right_motor = normal_speed - correction;
+    
+    if (left_motor < 0.0)  {left_motor=0.0;}
+    if (left_motor > 10.0) {left_motor=10.0;}
+    if (right_motor< 0.0)  {right_motor=0.0;}
+    if (right_motor> 10.0) {right_motor=10.0;}
+    
+    
+    motors[0]->setVelocity(left_motor);
+    motors[1]->setVelocity(right_motor);
+    
+    previous_error = error;
+    integral = I;  
+  }
 
-
-
-
-
-
-
+}
 
 
 
@@ -93,79 +203,12 @@ void turn_back(){
    motors[1]->setVelocity(0.0);
 }
 
-
-
-
-void follow_continous_line(){
-
-  int ir_readings[8];
-  double kp=1;
-  double kd=0.2;
-  double ki=0.02;
-  double prevoious_error = 0.0;
-  double integral = 0.0;
-  int coefficient[8]={-4000,-3000,-2000,-1000,1000,2000,3000,4000};
-   
-   
-  for (int i = 0; i < 8; i++) {
-    ir[i] = robot->getDistanceSensor(irNames[i]);
-    ir[i]->enable(TIME_STEP);
-  }
-  
-  while (robot->step(TIME_STEP) != -1) {
-    
-    bool exit = true;
-    
-    for (int i = 0; i < 8; i++) {
-        if (ir[i]->getValue() <150 ){
-          ir_readings[i]=0;
-          
-          }
-        else{
-          ir_readings[i]=1;
-          exit = false;
-        }  
-    } 
-    
-    if (exit){break;}
-   
-    double error = 0.0;
-    for (int j =0; j<8;j++){
-      error=error+ir_readings[j]*coefficient[j];
-    }
-    
-    double P=kp*error;
-    double I= (ki*error)+integral;
-    double D= kd*(error-prevoious_error);
-    
-    
-    double correction = (P+I+D)/1000;
-    double left_motor = normal_speed + correction;
-    double right_motor = normal_speed - correction;
-    
-    if (left_motor <0.0) {left_motor=0.0;}
-    if (left_motor >10.0) {left_motor=10.0;}
-    if (right_motor<0.0) {right_motor=0.0;}
-    if (right_motor>10.0) {right_motor=10.0;}
-    
-    
-    motors[0]->setVelocity(left_motor);
-    motors[1]->setVelocity(right_motor);
-    
-    prevoious_error = error;
-    integral = I;  
-  }
-
-}
-
-
-
 void solve_maze(){
 
   double kp=10;
   double kd=0.08;
   double ki=0.001;
-  double prevoious_error = 0.0;
+  double previous_error = 0.0;
   double integral = 0.0;
   
   
@@ -187,7 +230,7 @@ void solve_maze(){
         double error = (us_left- (us_right-40) )/4 + (98 -us_right)/12;
         double P=kp*error;
         double I= (ki*error)+integral;
-        double D= kd*(error-prevoious_error);
+        double D= kd*(error-previous_error);
    
         double correction = (P+I+D)/20;
         double left_motor = normal_speed + correction;
@@ -201,12 +244,12 @@ void solve_maze(){
         motors[0]->setVelocity(left_motor);
         motors[1]->setVelocity(right_motor);
         
-        prevoious_error = error;
+        previous_error = error;
         integral = I;
       }
       
       else if (us_front>240 && us_left>150){
-        prevoious_error = 0.0;
+        previous_error = 0.0;
         integral = 0.0;
         turn_left();
         while (robot->step(TIME_STEP) != -1){
@@ -219,7 +262,7 @@ void solve_maze(){
         }
         
       else if (us_front<240 && us_left>150){
-        prevoious_error = 0.0;
+        previous_error = 0.0;
         integral = 0.0;
         turn_left();
         while (robot->step(TIME_STEP) != -1){
@@ -232,7 +275,7 @@ void solve_maze(){
       }
       
       else if (us_front<150 && us_left<200){
-      prevoious_error = 0.0;
+      previous_error = 0.0;
       integral = 0.0;
       
       turn_right();
